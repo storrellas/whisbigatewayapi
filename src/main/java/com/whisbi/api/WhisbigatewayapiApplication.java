75e8414d2c13e05860e73f24f8daedd0bfc2d13c
s.torrellas <s.torrellas@whisbi.com>
2017-03-16 15:23:22 +0000

    ENH: Removed unused code

--- src/main/java/com/whisbi/api/WhisbigatewayapiApplication.java
+++ src/main/java/com/whisbi/api/WhisbigatewayapiApplication.java
@@ -1,7 +1,5 @@
 package com.whisbi.api;
 
-import java.io.BufferedReader;
-import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -9,22 +7,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.databind.node.TextNode;
 
 
 @RestController
@@ -33,37 +22,6 @@ public class WhisbigatewayapiApplication {
 
     private static final Logger log = LoggerFactory.getLogger(WhisbigatewayapiApplication.class);
 	
-    
-    private String getBody(HttpServletRequest request) {
-	  String body = "";
-	  if (request.getMethod().equals("POST") )
-	  {
-	    StringBuilder sb = new StringBuilder();
-	    BufferedReader bufferedReader = null;
-	
-	    try {
-	      bufferedReader =  request.getReader();
-	      char[] charBuffer = new char[128];
-	      int bytesRead;
-	      while ((bytesRead = bufferedReader.read(charBuffer)) != -1) {
-	        sb.append(charBuffer, 0, bytesRead);
-	      }
-	    } catch (IOException ex) {
-	      // swallow silently -- can't get body, won't
-	    } finally {
-	      if (bufferedReader != null) {
-	        try {
-	          bufferedReader.close();
-	        } catch (IOException ex) {
-	          // swallow silently -- can't get body, won't
-	        }
-	      }
-	    }
-	    body = sb.toString();
-	  }
-	  return body;
-	}
-    
     /*
     curl -i -H "Accept: application/json" -H "Content-Type: application/json" -X POST 
     -d '{"agent_id":"587A179B-7097-46EC-B2E8-89D435975252"}' 
@@ -80,7 +38,7 @@ public class WhisbigatewayapiApplication {
 		
 		// Login request to LM 
 		RestTemplate restTemplate = new RestTemplate(); 
-		String str = restTemplate.postForObject("http://localhost:8011/", event_login, String.class); 
+		String str = restTemplate.postForObject("http://localhost:8011/", event_login, String.class);
 		log.info(str);
 		return str;
 	}
