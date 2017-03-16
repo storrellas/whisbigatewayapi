42e59fc667bdbac27c27e1938beb046bf58a7152
s.torrellas <s.torrellas@whisbi.com>
2017-03-16 14:01:34 +0000

    WIP: First approach

--- src/main/java/com/whisbi/api/WhisbigatewayapiApplication.java
+++ src/main/java/com/whisbi/api/WhisbigatewayapiApplication.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.node.TextNode;
@@ -31,45 +34,74 @@ public class WhisbigatewayapiApplication {
 	
     
     private String getBody(HttpServletRequest request) {
-    	  String body = "";
-    	  if (request.getMethod().equals("POST") )
-    	  {
-    	    StringBuilder sb = new StringBuilder();
-    	    BufferedReader bufferedReader = null;
-
-    	    try {
-    	      bufferedReader =  request.getReader();
-    	      char[] charBuffer = new char[128];
-    	      int bytesRead;
-    	      while ((bytesRead = bufferedReader.read(charBuffer)) != -1) {
-    	        sb.append(charBuffer, 0, bytesRead);
-    	      }
-    	    } catch (IOException ex) {
-    	      // swallow silently -- can't get body, won't
-    	    } finally {
-    	      if (bufferedReader != null) {
-    	        try {
-    	          bufferedReader.close();
-    	        } catch (IOException ex) {
-    	          // swallow silently -- can't get body, won't
-    	        }
-    	      }
-    	    }
-    	    body = sb.toString();
-    	  }
-    	  return body;
-    	}
+	  String body = "";
+	  if (request.getMethod().equals("POST") )
+	  {
+	    StringBuilder sb = new StringBuilder();
+	    BufferedReader bufferedReader = null;
+	
+	    try {
+	      bufferedReader =  request.getReader();
+	      char[] charBuffer = new char[128];
+	      int bytesRead;
+	      while ((bytesRead = bufferedReader.read(charBuffer)) != -1) {
+	        sb.append(charBuffer, 0, bytesRead);
+	      }
+	    } catch (IOException ex) {
+	      // swallow silently -- can't get body, won't
+	    } finally {
+	      if (bufferedReader != null) {
+	        try {
+	          bufferedReader.close();
+	        } catch (IOException ex) {
+	          // swallow silently -- can't get body, won't
+	        }
+	      }
+	    }
+	    body = sb.toString();
+	  }
+	  return body;
+	}
     
+    public static class Agent{
+        public String getAgent_id() {
+			return agent_id;
+		}
+
+		public void setAgent_id(String agent_id) {
+			this.agent_id = agent_id;
+		}
+
+		public String agent_id;
+    }
+        
     @RequestMapping(value="/api/login/", method=RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.OK)
-    public String login(HttpServletRequest request) 
-    {
-    	log.info("Request URL: " + request.getRequestURL().toString() + " " + request.getQueryString());
-    	log.info("Request URL: " + request.getRequestURL().toString() + " " + getBody(request));
-    	//log.info("Request URL: " + request.getRequestURL().toString() + " " + agent_id.toString());
-   	
+    public String login(HttpEntity<String> httpEntity, HttpServletRequest request) 
+    {    	
+    	log.info("Body: " + httpEntity.getBody());
+    	
+
+    	try {
+        	//create ObjectMapper instance
+        	ObjectMapper objectMapper = new ObjectMapper();
+        	//convert json string to object
+        	WhisbigatewayapiApplication.Agent agent = objectMapper.readValue(httpEntity.getBody(), WhisbigatewayapiApplication.Agent.class);
+			
+	    	log.info("Body: " + agent.agent_id);
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     	
     	
+    	/*
     	// Login request to LM
     	ObjectMapper mapper = new ObjectMapper();
     	ObjectNode login_event = mapper.createObjectNode();
@@ -77,9 +109,11 @@ public class WhisbigatewayapiApplication {
     	login_event.put("agent_id", "587A179B-7097-46EC-B2E8-89D435975252");
         RestTemplate restTemplate = new RestTemplate();      
         String str = restTemplate.postForObject("http://localhost:8011/", login_event, String.class);
-        //log.info(str);
-          	    	
-    	return str;
+        //log.info(str);          	    
+    	return str;        
+        /**/
+        
+    	return "ok";
     }
     
 
